5597719bb04d0c7f79b86e9cfdc0d6165d0520a5
Dipak Khade <dipakhade214@gmail.com>
2026-02-10 12:19:50 +0530

    posts mod init

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use poem::{get, handler, listener::TcpListener, web::Path, Route, Server};
+pub mod routes;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let app = Route::new().at("/hello/:name", get(hello));
+    let app = Route::new()
+    .nest("/posts", );
     Server::new(TcpListener::bind("0.0.0.0:3000"))
       .run(app)
       .await
