9318ad7d87103ca28b3224654bccb6420c606450
Dipak Khade <dipakhade214@gmail.com>
2026-02-12 15:14:24 +0530

    added users struct

--- src/routes/users/all_users.rs
+++ src/routes/users/all_users.rs
@@ -1,4 +1,29 @@
 
 struct User {
-    
+    id: i32,
+    name: String,
+    email: String,
+    address: Address,
+    phone: String,
+    website: String,
+    company: Company
+}
+
+struct Address {
+    street: String,
+    suite: String,
+    city: String,
+    zipcode: String,
+    geo: GeoLocation
+}
+
+struct GeoLocation {
+    lat: String,
+    lng: String
+}
+
+struct Company {
+    name: String,
+    catchPhrase: String,
+    bs: String
 }
