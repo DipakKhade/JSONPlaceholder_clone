838fafd211eb07aa80053bc7ba17578feaf96d7d
Dipak Khade <dipakhade214@gmail.com>
2026-02-16 21:09:00 +0530

    added routes

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ pub mod routes;
 async fn main() -> Result<(), std::io::Error> {
     let app = Route::new()
     .nest("/posts", routes::posts::posts_routes::posts_routes())
-    .nest("/user", routes::users::users_routes::users_route());
+    .nest("/users", routes::users::users_routes::users_route())
+    .nest("/todos", routes::todos::todos_routes::todos_routes())
+    .nest("/comments", routes::comments::comments_routes::comments_routes())
+    .nest("/albums", routes::albums::albums_routes::albums_routes())
+    .nest("/photos", routes::photos::photos_routes::photos_routes());
     Server::new(TcpListener::bind("0.0.0.0:3000"))
       .run(app)
       .await
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,3 +1,7 @@
 
 pub mod posts;
 pub mod users;
+pub mod todos;
+pub mod comments;
+pub mod albums;
+pub mod photos;
